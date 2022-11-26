51e3e1513a0c5e61fca25724638423b5b8314ddf
Vitaliy Mischenko <vitas.mishenko@gmail.com>
2022-11-26 11:08:04 +0700

    header in progress

---
+++ src/components/Header/index.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+
+const Header = () => {
+    const [language, setLanguage] = useState('ru');
+    const [dropdown, setDropdown] = useState(false);
+
+    return (
+        <header className="header">
+            <div className="header__left">
+                <div className="header__logo">
+                    <img src={'../images/logo.png'} alt="logo" />
+                </div>
+                <div className="language">
+                    <span
+                        className="language__selected"
+                        onClick={() => setDropdown(!dropdown)}
+                    >
+                        <img src={`../images/${language}.svg`} alt={language} />
+                    </span>
+                    {dropdown && (
+                        <div className="language__dropdown">
+                            <div className="language__option">
+                                <div
+                                    className="language__flag"
+                                    onClick={() => {
+                                        setLanguage('ru');
+                                        setDropdown(false);
+                                    }}
+                                >
+                                    <img src={`../images/ru.svg`} alt="ru" />
+                                </div>
+                                <p className="language__name">Русский</p>
+                            </div>
+                            <div className="language__option">
+                                <div
+                                    className="language__flag"
+                                    onClick={() => {
+                                        setLanguage('us');
+                                        setDropdown(false);
+                                    }}
+                                >
+                                    <img src={`../images/us.svg`} alt="us" />
+                                </div>
+                                <p className="language__name">English</p>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+            <div className="header__right"></div>
+        </header>
+    );
+};
+
+export default Header;
